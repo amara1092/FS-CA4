3a0ae8f04705ba1d6f94c412cde3bac9f1973220
Amara Uzoma <amara1092@gmail.com>
2020-05-10 12:15:51 +0100

    Test to see if my git is working

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default function App (){
+    return (
+      <View>
+        <Text>
+            Open
+        </Text>
+      </View>
+    );
+  }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
