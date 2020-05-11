1fc516500cd9b6e0f4052d8237d50de554f29038
Amara Uzoma <amara1092@gmail.com>
2020-05-11 11:11:47 +0100

    Created a screens to view on the homeStack

---
+++ routes/homeStack.js
@@ -0,0 +1,18 @@
+import{ createStackNavigator } from 'react-navigation-stack';
+import{ createAppContainer } from 'react-navigation';
+import Home from '../screens/home';
+import Shoes from '../screens/Shoes';
+const screens = {
+Home:{
+    screen: Home
+},
+
+Shoes: {
+        screen: Shoes
+    }
+
+}
+
+const HomeStack = createStackNavigator(screens);
+
+export default createAppContainer(HomeStack);
