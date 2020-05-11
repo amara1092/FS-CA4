fb63655f6b7c618f23142db21b294b3fb6b5d484
Amara Uzoma <amara1092@gmail.com>
2020-05-11 22:38:30 +0100

    Added a review section

--- screens/Shoes.js
+++ screens/Shoes.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/card';
 
 export default function Shoes({ navigation }) {
     return (
         <View>
             <Text style={globalStyles.container}>Shoes</Text>
+            <Card>
             <Text>{ navigation.getParam('title') }</Text>
             <Text>{navigation.getParam('body')}</Text>
             <Text>{navigation.getParam('rating')}</Text>
+        </Card>
         </View>
     )
 }
